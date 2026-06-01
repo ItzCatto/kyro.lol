2149ccf1a392316361b97ae3c45762d5b99f864c
Whispr <cattoofr@gmail.com>
2026-06-01 16:28:24 -0500

    Add BloodSync link to links.ts

--- artifacts/kyro/src/data/links.ts
+++ artifacts/kyro/src/data/links.ts
@@ -9,4 +9,5 @@ export const links: KyroLink[] = [
   { slug: "steamrip", title: "SteamRip", url: "https://steamrip.com/" },
   { slug: "fitgirl", title: "FitGirl", url: "https://fitgirl-repacks.site" },
   { slug: "solara", title: "Solara", url: "https://getsolara.dev/download/static/files/BootstrapperNew.exe" },
+  { slug: "bloodsync", title: "BloodSync", url: "https://www.youtube.com/watch?v=UzyzAqQ3DXI" },
 ];
