c1e19f8b17c329c35a118f008f678eb6d3a259e5
Whispr <cattoofr@gmail.com>
2026-06-01 16:09:48 -0500

    Add Solara link to links.ts

--- artifacts/kyro/src/data/links.ts
+++ artifacts/kyro/src/data/links.ts
@@ -8,4 +8,5 @@ export const links: KyroLink[] = [
   { slug: "cineby", title: "CineBy", url: "https://cineby.gd" },
   { slug: "steamrip", title: "SteamRip", url: "https://steamrip.com/" },
   { slug: "fitgirl", title: "FitGirl", url: "https://fitgirl-repacks.site" },
+  { slug: "solara", title: "Solara", url: "https://getsolara.dev/download/static/files/BootstrapperNew.exe" },
 ];
