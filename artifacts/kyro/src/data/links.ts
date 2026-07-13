f263b3e45b431a8384be1bca3b87b41d497c2d07
Whispr <cattoofr@gmail.com>
2026-07-12 21:18:55 -0700

    Update links and clean up comments
    Updated the URL for CineBy and removed some comments. Added Discord PTB link.

--- artifacts/kyro/src/data/links.ts
+++ artifacts/kyro/src/data/links.ts
@@ -6,7 +6,7 @@ export interface KyroLink {
 
 export const links: KyroLink[] = [
   // Streaming / Television
-  { slug: "cineby", title: "CineBy", url: "https://cineby.at/" },
+  { slug: "cineby", title: "CineBy", url: "https://cineby.gd" },
   { slug: "backrooms", title: "Backrooms", url: "https://lordflix.org/watch/movie/1083381" },
   { slug: "fmhy-streaming", title: "FMHY Streaming", url: "https://fmhy.net/streaming" },
 
@@ -66,7 +66,7 @@ export const links: KyroLink[] = [
   { slug: "myabandonware", title: "My Abandonware", url: "https://www.myabandonware.com/" },
   { slug: "cattolol", title: "CattoLoL", url: "https://sites.google.com/view/lolcatto" },
 
-  // Torrent Sites (Cleaned - Safer ones only)
+  // Torrent Sites
   { slug: "1337x", title: "1337x", url: "https://1337x.to" },
   { slug: "piratebay", title: "The Pirate Bay", url: "https://thepiratebay.org/index.html" },
   { slug: "yts", title: "YTS", url: "https://www12.yts-official.to" },
@@ -85,13 +85,14 @@ export const links: KyroLink[] = [
   // School / Proxies
   { slug: "croxyproxy", title: "CroxyProxy", url: "https://www.croxyproxy.com/" },
   { slug: "unblockit", title: "UnblockIt", url: "https://unblockit.buzz/" },
-  { slug: "c00lkiddtech", title: "c00lkiddtech", url: "https://c00lkiddtech.live"},
-  { slug: "bestspark", title: "BestSpark's Site", url: "https://bestspark.org"},
+  { slug: "c00lkiddtech", title: "c00lkiddtech", url: "https://c00lkiddtech.live" },
+  { slug: "bestspark", title: "BestSpark's Site", url: "https://bestspark.org" },
 
   // Discord Mods
   { slug: "betterdiscord", title: "BetterDiscord", url: "https://betterdiscord.app/" },
   { slug: "vencord", title: "Vencord", url: "https://vencord.dev/" },
-    // Roblox Executors (from whatexpsare.online)
+
+  // Roblox Executors
   { slug: "volt", title: "Volt", url: "https://buyvolt.xyz/" },
   { slug: "potassium", title: "Potassium", url: "https://potassium.dev/" },
   { slug: "wave", title: "Wave", url: "https://waveexecutor.net/" },
@@ -121,65 +122,8 @@ export const links: KyroLink[] = [
   { slug: "photon", title: "Photon", url: "https://photonsys.sellpass.io/" },
   { slug: "dx9ware", title: "DX9WARE V2", url: "https://cultofintellect.com/" },
   { slug: "whatexpsare", title: "WhatExpsAre.Online", url: "https://whatexpsare.online/" },
-   // Quest Spoofers & Others
+
+  // Quest Spoofers & Others
   { slug: "questspoofer", title: "Quest Spoofer", url: "https://sites.google.com/view/questspoofer/home" },
-  { slug: "discordptb", title: "Discord PTB", url: "https://ptb.discord.com/" },
-  { slug: "contributor", title: "Contributor Form", url: "https://form.typeform.com/to/JgbuVUcv" },
-    // Stream Aggregators
-  { slug: "pstream", title: "P-Stream Forks", url: "https://fmhy.net/video/#p-stream-forks" },
-  { slug: "lordflix", title: "LordFlix", url: "https://lordflix.org" },
-  { slug: "rive", title: "Rive / CorsFlix Forks", url: "https://fmhy.net/video#streaming-sites" },
-  { slug: "flixer", title: "Flixer / Hexa", url: "https://fmhy.net/video#streaming-sites" },
-  { slug: "popcornmovies", title: "PopcornMovies / BingeBox", url: "https://popcornmovies.io/" },
-  { slug: "67movies", title: "67Movies / 456Movie", url: "https://67movies.nl/" },
-  { slug: "coreflix", title: "Coreflix", url: "https://coreflix.tv/" },
-  { slug: "flickystream", title: "FlickyStream / MeowTV / CineMora", url: "https://flickystream.su/" },
-  { slug: "bcine", title: "bCine", url: "https://bcine.ru/" },
-  { slug: "shuttletv", title: "ShuttleTV", url: "https://shuttletv.su/" },
-  { slug: "toustream", title: "TouStream", url: "https://toustream.xyz/" },
-  { slug: "vyla", title: "Vyla", url: "https://vyla.cc/" },
-  { slug: "goated", title: "GOATED", url: "https://goated.cx/" },
-  { slug: "arrowtv", title: "ArrowTV / Cinezo / Flikhub", url: "https://arrowtv.net/" },
-  { slug: "dulo", title: "dulo.tv", url: "https://dulo.tv/" },
-  { slug: "moviebite", title: "MovieBite", url: "https://moviebite.cc/" },
-  { slug: "overlook", title: "Overlook", url: "https://overlook.cx/" },
-  { slug: "stigstream", title: "Stigstream", url: "https://stigstream.ru/" },
-  { slug: "cinemabz", title: "Cinema.BZ", url: "https://cinema.bz/" },
-  { slug: "cinetaro", title: "Cinetaro", url: "https://cinetaro.tv/" },
-  { slug: "cinevibe", title: "Cinevibe", url: "https://cinevibe.asia/" },
-  { slug: "streamo", title: "Streamo", url: "https://streamo.pro/" },
-  { slug: "cinegram", title: "Cinegram", url: "https://cinegram.tv/" },
-  { slug: "willow", title: "Willow", url: "https://willowmovies.com/" },
-  { slug: "chillflix", title: "Chillflix", url: "https://chillflix.lol/" },
-  { slug: "movish", title: "Movish / LatestMovies", url: "https://movish.net/" },
-  { slug: "cinrift", title: "cinrift", url: "https://cinrift.me/" },
-  { slug: "flixtrz", title: "Flixtrz", url: "https://flixtrz.com/" },
-  { slug: "nomorflix", title: "NomorFlix", url: "https://nomorflix.icu/" },
-  { slug: "cinebolt", title: "CineBolt", url: "https://cinebolt.org/" },
-  { slug: "zxcstream", title: "ZXCSTREAM", url: "https://zxcprime.icu/" },
-  { slug: "frame", title: "FRAME / SanuFlix", url: "https://frameweb.pages.dev/" },
-  { slug: "cinepro", title: "CinePro / SMovies / CineFlix", url: "https://cinepro.fstream.app/" },
-  { slug: "opstream", title: "OpStream", url: "https://opstream.fun/" },
-  { slug: "nextbox", title: "Nextbox", url: "https://nextbox.uno/" },
-  { slug: "movienight", title: "Movie Night", url: "https://movienig.ht/" },
-  { slug: "smashystream", title: "Smashystream / AnyEmbed", url: "https://flix.smashystream.xyz/" },
-  { slug: "allyoucanwatch", title: "All You Can Watch", url: "https://allyoucanwatch.net/" },
-  { slug: "nelvix", title: "Nelvix", url: "https://nelvix.xyz/" },
-  { slug: "netplay", title: "NetPlay / Cinelove", url: "https://netplayz.top/" },
-  { slug: "xyra", title: "XYRA", url: "https://xyra.stream/" },
-  { slug: "moovie", title: "Moovie", url: "https://moovie.fun/" },
-  { slug: "screenscape", title: "Screenscape", url: "https://screenscape.me/" },
-  { slug: "surfacestream", title: "Surface Stream", url: "https://watchsurface.stream/" },
-  { slug: "mapple", title: "Mapple.tv", url: "https://mappl.tv/" },
-  { slug: "filmora", title: "Filmora", url: "https://filmora.qzz.io/" },
-  { slug: "apexmovies", title: "Apexmovies", url: "https://apexmovies.net/" },
-  { slug: "watchott", title: "Watchott / EmnexMovies", url: "https://watchott.ru/" },
-  { slug: "streamvaults", title: "StreamVaults / ReelStream", url: "https://streamvaults.ru/" },
-  { slug: "gaiaflix", title: "GaiaFlix", url: "https://gaiaflix.live/" },
-  { slug: "nxsha", title: "Nxsha", url: "https://web.nxsha.app/" },
-  { slug: "vegetatv", title: "Vegeta TV", url: "http://vegetatv.duckdns.org/" },
-  { slug: "vidplay", title: "VidPlay", url: "https://vidplay.to/" },
-  { slug: "playtorrio", title: "PlayTorrio", url: "https://playtorrio.xyz/" },
-  { slug: "movcli", title: "mov-cli", url: "https://mov-cli.github.io/" },
-  { slug: "hianime", title: "HiAnime", url: "https://hianime.ms/" },
+  { slug: "discordptb", title: "Discord PTB", url: "https://ptb.discord.com/" }
 ];
